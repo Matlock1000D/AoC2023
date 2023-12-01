4ca4171456cf25b2e1c5de456f9872c29b069524
lpjapp@gmail.com <lpjapp@gmail.com>
2023-12-01 10:14:12 +0200

    First commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace AoC2023;
+
+partial class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length != 3)    //argumenttien pitäisi olla [päivä] [kohta] [datatiedosto], heitä virhe jos niitä on väärä määrä
+        {
+            Console.WriteLine("Anna argumentit: päivä kohta datatiedosto");
+            return;
+        }
+
+        //Virheenhallinta olkoon vähän niin ja näin, koska emmehän tee virheitä.
+
+        int day = int.Parse(args[0]);
+        int phase = int.Parse(args[1]);
+        string datafile = args[2];
+
+        switch (day)
+        {
+            case 1:
+                Day1(phase, datafile);
+                break;
+        }
+    }
+}
---
+++ day1.cs
@@ -0,0 +1,9 @@
+namespace AoC2023;
+
+partial class Program
+{
+    static void Day1(int phase, string datafile)
+    {
+        Console.WriteLine("Mitä äijä!");
+    }
+}
