78e743bc153de8c429c431437ad69d28d8ee37fa
lpjapp@gmail.com <lpjapp@gmail.com>
2023-12-14 23:49:43 +0200

    Day 13b

--- day13.cs
+++ day13.cs
@@ -6,7 +6,7 @@ using System.Text;
 
 partial class Program
 {
-    static int MirrorFinder(List<string> pattern)
+    static int MirrorFinder(List<string> pattern, int nogoodresult = -1)
     {
         for (int i = 1;i<pattern.Count;i++)
             {
@@ -19,15 +19,39 @@ partial class Program
                     break;
                 }
             }
-            if (ismirrored) return i;
+            if (ismirrored && nogoodresult != i) return i;
         }
         return 0;
+    }
+
+    static int FindMirrorrow(List<string> pattern, int nogoodresult = -1)
+    {
+            //Ensin riveittäin
+            int mirrorrow = MirrorFinder(pattern, nogoodresult/100);
+            if (mirrorrow != 0) return mirrorrow * 100;  //turha katsoja sarakkeita, jos asia on jo ratkennut
+
+            //sitten sarakkeisiin
+            //transponoidaan rivit
+            var t_pattern = new List<string>();
+            for (int i=0; i<pattern[0].Length; i++)
+            {
+                var t_rowbuilder = new StringBuilder();
+                for (int j=0; j<pattern.Count; j++)
+                {
+                    t_rowbuilder.Append(pattern[j][i]);
+                }
+                t_pattern.Add(t_rowbuilder.ToString());
+            }
+            //sitten sama homma
+            mirrorrow = MirrorFinder(t_pattern, nogoodresult);
+            return mirrorrow; 
     }
 
     static BigInteger Day13(int phase, string datafile)
     {
         string[] lines = [];
         BigInteger result = 0;
+        var originalresults = new List<int>();
 
         if (File.Exists(datafile))
         {
@@ -63,27 +87,40 @@ partial class Program
         //Käydään listat läpi
         foreach (var pattern in patterns)
         {
-            //Ensin riveittäin (tee tämä metodiksi)
-            int mirrorrow = MirrorFinder(pattern);
-            result += 100 * mirrorrow;
-            if (mirrorrow != 0) continue;  //turha katsoja sarakkeita, jos asia on jo ratkennut
-
-            //sitten sarakkeisiin
-            //transponoidaan rivit
-            var t_pattern = new List<string>();
-            for (int i=0; i<pattern[0].Length; i++)
+            int mirrorrow = FindMirrorrow(pattern);
+            if (phase == 1) result += mirrorrow;
+            else originalresults.Add(mirrorrow);
+        }
+        if (phase == 2)
+        {
+            for (int i1 = 0; i1 < patterns.Count; i1++)
             {
-                var t_rowbuilder = new StringBuilder();
-                for (int j=0; j<pattern.Count; j++)
+                List<string>? pattern = patterns[i1];
+                bool patterdone = false;
+                for (int i = 0; i < pattern.Count;i++)
                 {
-                    t_rowbuilder.Append(pattern[j][i]);
+                    for (int j=0; j<pattern[0].Length;j++)
+                    {
+                        var temppattern = pattern.ToList();
+                        var arr_temppattern = temppattern[i].ToCharArray();
+                        if (arr_temppattern[j] == '.') arr_temppattern[j] = '#';
+                        else arr_temppattern[j] = '.';
+                        temppattern[i] = new string(arr_temppattern);
+                        int newmirrorline = FindMirrorrow(temppattern, originalresults[i1]);
+
+                        if (newmirrorline != 0)
+                        {
+                            result += newmirrorline;
+                            patterdone = true;
+                            break;
+                        }
+                    }
+                    if (patterdone) break;
                 }
-                t_pattern.Add(t_rowbuilder.ToString());
             }
-            //sitten sama homma
-            result += MirrorFinder(t_pattern);
         }
         return result;
     }
-
 }
+
+//403 too low
