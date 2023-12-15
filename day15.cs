1d2b670e8560c2e2463cee9b8212728653fcb74e
lpjapp@gmail.com <lpjapp@gmail.com>
2023-12-16 00:41:32 +0200

    Day 15b

--- day15.cs
+++ day15.cs
@@ -1,10 +1,9 @@
 namespace AoC2023;
 
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Numerics;
-using System.Text;
-using System.Xml;
+using System.Collections.Specialized;
+using System.Collections;
 
 partial class Program
 {
@@ -48,7 +47,41 @@ partial class Program
 
         var sequence = lines[0].Split(',');
         result = 0;
-        foreach(var step in sequence) result += Hasher(step);
+        if (phase == 1) foreach(var step in sequence) result += Hasher(step);
+        else
+        {
+            var boxes = new Dictionary<int,OrderedDictionary>();
+            for (int i = 0; i<256;i++)
+            {
+                boxes[i] = new OrderedDictionary();
+            }
+
+            foreach(var instruction in sequence)
+            {
+                int commandindex = instruction.Select((c, i) => c == '=' || c == '-' ? i : -1).Where(i => i != -1).ToList()[0];
+                char command = instruction[commandindex];
+                string label = instruction[..commandindex];
+                int box = Hasher(label);
+                int lens;
+                //palaset on parsetettu, operoidaan
+                if (command == '=') 
+                {
+                    lens = instruction[commandindex+1];
+                    boxes[box][label] = lens;
+                }
+                if (command == '-') boxes[box].Remove(label);
+            }
+        
+            //haetaan vastaus
+            for (int i=0;i<256;i++)
+            {
+                var lenslist = boxes[i].Cast<DictionaryEntry>().ToList();
+                for (int j=0;j<lenslist.Count;j++)
+                {                
+                    result += (i+1)*(j+1)*((int) lenslist[j].Value-48);
+                }
+            }
+        }
 
         return result;
     } 
